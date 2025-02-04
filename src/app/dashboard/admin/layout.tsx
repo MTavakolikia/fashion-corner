e3f0ae0cef142c0f01d606ccacf37d2842a21f08
mohammadtk1994 <mohammadtavakolikia66@gmail.com>
2025-02-04 21:18:36 +0330

    add dashboard layout and header for dashboard

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from "react";
 
 export default function AdminDashboardLayout({ children }: { children: ReactNode }) {
     return (
-        <div>{children}</div>
+        <div className="w-full h-full">{children}</div>
     )
 }
--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+import Header from "@/components/dashboard/header/header";
+import Sidebar from "@/components/dashboard/sidebar/sidebar";
 
-export default function AdminDashboardPage() {
+export default async function AdminDashboardLayout({
+    children,
+}: {
+    children: ReactNode;
+}) {
+    // Block non admins from accessing the admin dashboard
+    const user = await currentUser();
+    if (!user || user.privateMetadata.role !== "ADMIN") redirect("/");
     return (
-        <div>Admin DashboardPage</div>
-    )
+        <div className="w-full h-full">
+            {/* Sidebar */}
+            <Sidebar isAdmin />
+            <div className="ml-[300px]">
+                {/* Header */}
+                <Header />
+                <div className="w-full mt-[75px] p-4">{children}</div>
+            </div>
+        </div>
+    );
 }
